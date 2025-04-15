58f8b471765a23b7e2eb7f8abde3903912b928a0
reigadegr <272268642@qq.com>
2025-04-15 14:43:35 +0800

    chore: 上传文件

---
+++ release_create.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+tag_name="v2.1.0"
+tag_description="Release v2.1.0"
+
+git tag -a "v2.1.0" -m "Release v2.1.0"
+git push origin "v2.1.0"
+gh release create "$tag_name" --title "$tag_description" --notes-file "module/changelog.md" "./video.zip"
